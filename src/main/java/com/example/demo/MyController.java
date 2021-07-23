4f261430c53ce4f46693bf571227f0543d46cc48
Rohan Jaiswal <rohan.jaiswal@geotechinfo.net>
2021-07-23 10:06:03 +0530

    made changes to the controller class

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -9,6 +9,6 @@ public class MyController {
 
     @RequestMapping("/api")
     public String hello(@RequestParam("name") String name) {
-        return "Hello there" + name;
+        return "Hello there! " + name;
     }
 }
