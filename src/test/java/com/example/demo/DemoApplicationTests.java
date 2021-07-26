1571e75fd2beb4fb8a4addb448ad7db70a488fd3
Rohan Jaiswal <rohan.jaiswal@geotechinfo.net>
2021-07-26 17:37:51 +0530

    added demo test case

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,12 +2,16 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @SpringBootTest
 class DemoApplicationTests {
 
     @Test
     void contextLoads() {
+        DemoApplication demoApplication = new DemoApplication();
+        assertThat(demoApplication.DemoTestCase()).isEqualTo(true);
     }
 
 }
